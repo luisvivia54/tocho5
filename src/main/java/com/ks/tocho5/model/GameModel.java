335bcedc2b046d9d8947bc3227177d4d9f97aee5
Luis Vivia <luisvivia@MacBook-Pro-de-Luis-10.local>
2025-10-25 18:02:07 -0600

    prueba1 partido/update

--- src/main/java/com/ks/tocho5/model/GameModel.java
+++ src/main/java/com/ks/tocho5/model/GameModel.java
@@ -2,8 +2,10 @@ package com.ks.tocho5.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "game_score")
 public class GameModel{
 	@Override
 	public String toString() {
