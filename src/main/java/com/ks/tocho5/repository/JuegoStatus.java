b647e313a225f892578fda570beb7d90f0293d60
Luis Vivia <luisvivia@MacBook-Pro-de-Luis-10.local>
2025-11-08 17:03:09 -0600

    se agregan ultimos 5 partidos

--- src/main/java/com/ks/tocho5/repository/JuegoStatus.java
+++ src/main/java/com/ks/tocho5/repository/JuegoStatus.java
@@ -51,9 +51,9 @@ public interface JuegoStatus extends JpaRepository<GameStatusModel, Integer> {
 		  join fetch g.homeTeam
 		  join fetch g.awayTeam
 		  where g.status = :status
-		  order by g.lastUpdate desc, g.id desc
+		  order by g.updated_at desc, g.id desc
 		""")
-		List<GameStatusModel> findFinalWithTeams(@Param("status") String status, Pageable pageable);
+  List<GameStatusModel> findFinalWithTeams(@Param("status") String status, Pageable pageable);
 
 
   @Query("""
