6ed65ee17be7eeb4a0dc49b5d79f1cb68c08fb37
Luis Vivia <luisvivia@MacBook-Pro-de-Luis-10.local>
2025-12-08 22:10:47 -0600

    se agrego logica de imagenes

--- src/main/java/com/ks/tocho5/config/SecurityConfig.java
+++ src/main/java/com/ks/tocho5/config/SecurityConfig.java
@@ -1,7 +1,9 @@
+// src/main/java/com/ks/tocho5/config/SecurityConfig.java
 package com.ks.tocho5.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,28 +16,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
             // CORS y CSRF
             .cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
 
-            // Qué endpoints son públicos y cuáles requieren auth
+            // 🔓 TODO el mundo puede acceder a TODO
             .authorizeHttpRequests(auth -> auth
-            		.requestMatchers(
-                            "/api/teams",              // lista de equipos
-                            "/api/games",              // partidos
-                            "/api/gamesFinal",         // últimos 5
-                            "/api/points",             // tabla de posiciones
+                .anyRequest().permitAll()
+            );
 
-                            // si ya agregaste estos:
-                            "/api/teams/*/detail",     // detalle público de equipo
-                            "/api/teams/*/players"     // jugadores de un equipo (solo lectura)
-                        ).permitAll()
-                .anyRequest().authenticated()
-            )
-
-            // Resource Server con JWT (Keycloak)
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+            // 👇 IMPORTANTE: por ahora SIN resource server
+            // .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
     }
